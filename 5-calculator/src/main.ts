a959e5a30700eb70a8f482390871b46a1f44d25a
Gabriel Sena <stardusteight.d4cc@gmail.com>
2023-02-02 21:54:17 -0300

    buttonsContainer addEventListener

--- 5-calculator/src/main.ts
+++ 5-calculator/src/main.ts
@@ -4,14 +4,29 @@ import { buttonsProps } from './utils/buttonsProps'
 const ELEMENTS_ID = {
   app: 'app',
   buttonsContainer: 'buttonsContainer',
+  operation: 'operation',
+  result: 'result',
 }
 
-const rendersButtons = (buttonsContainer: HTMLButtonElement) => {
+const rendersButtons = (buttonsContainer: HTMLDivElement) => {
   buttonsProps.map((button) => {
+    const props = button?.props
     const prevState = buttonsContainer.innerHTML
-    buttonsContainer.innerHTML =
-      prevState +
-      `<button type="button" class="${button.styles} button col-span-1">${button.content}</button>`
+    const newChild = `<button type="button" class="${button.styles} button col-span-1">${button.content}</button>`
+    if (props) {
+      const attributes: string[] = []
+      Object.entries(props).forEach(([key, value]: any) => {
+        attributes.push(`${key}="${value}"`)
+      })
+      const newChild = `<button type="button" ${attributes
+        .toLocaleString()
+        .replace(',', '')} class="${button.styles} button col-span-1">${
+        button.content
+      }</button>`
+      buttonsContainer.innerHTML = prevState + `${newChild}`
+    } else {
+      buttonsContainer.innerHTML = prevState + `${newChild}`
+    }
   })
 }
 
@@ -19,14 +34,13 @@ document.querySelector<HTMLDivElement>(`#${ELEMENTS_ID.app}`)!.innerHTML = `
   <div class="flex items-center justify-center h-screen overflow-hidden">
     <main class="calculatorContainer">
       <div class="flex flex-col pt-[54px] px-[54px] gap-y-2">
-        <div class="operation text-right !leading-[140%] text-[20px] text-[#6B6B6B]">
-          1 + 1
+        <div id=${ELEMENTS_ID.operation} class="text-right !leading-[140%] text-[20px] text-[#6B6B6B]">
         </div>
         <div class="flex items-center justify-between">
           <span class="text-[#6B6B6B]">
             <img src="/src/assets/equals.svg" />
           </span>
-          <h2 class="result text-[36px] !leading-[140%]">0</h2>
+          <h2 id=${ELEMENTS_ID.result} class="text-[36px] !leading-[140%]">0</h2>
         </div>
       </div>
       <div
@@ -37,6 +51,68 @@ document.querySelector<HTMLDivElement>(`#${ELEMENTS_ID.app}`)!.innerHTML = `
     </main>
   </div>
 `
-rendersButtons(
-  document.querySelector<HTMLButtonElement>(`#${ELEMENTS_ID.buttonsContainer}`)!
-)
+
+const buttonsContainer = document.querySelector<HTMLDivElement>(
+  `#${ELEMENTS_ID.buttonsContainer}`
+)!
+const operation = document.querySelector(
+  `#${ELEMENTS_ID.operation}`
+) as HTMLDivElement
+const resultElement = document.querySelector(
+  `#${ELEMENTS_ID.result}`
+) as HTMLHeadingElement
+
+rendersButtons(buttonsContainer)
+
+let operationState: any[] = []
+console.log(operationState)
+
+function equals() {
+  let number = ''
+  operationState.map((operationValue) => {
+    if (Number(operationValue) >= 0 || Number(operationValue <= 9)) {
+      number += operationValue.toLocaleString()
+    } else if (operationValue === 'ADDITION') {
+      number += '+'
+    }
+    else if (operationValue === 'SUBTRACTION') {
+      number += '-'
+    }
+  })
+  return eval(number)
+}
+
+buttonsContainer.addEventListener('click', (event) => {
+  const button = event.target as HTMLButtonElement
+  const prevState = operation.innerHTML
+
+  // depois passar para funções
+  if (Number(button.value) >= 0 || Number(button.value) <= 9) {
+    operation.innerHTML = prevState + button.value
+    operationState.push(button.value)
+  }
+
+
+  if (
+    button.value === 'ADDITION' &&
+    !isNaN(operationState[operationState.length - 1])
+  ) {
+    operation.innerHTML = prevState + ' + '
+    operationState.push('ADDITION')
+  }
+  if (
+    button.value === 'SUBTRACTION' &&
+    !isNaN(operationState[operationState.length - 1])
+  ) {
+    operation.innerHTML = prevState + ' - '
+    operationState.push('SUBTRACTION')
+  }
+  if (button.value === 'CLEAR') {
+    operation.innerHTML = ''
+    operationState = []
+  }
+  if (button.value === 'EQUAL') {
+    const result = equals()
+    resultElement.innerHTML = result
+  }
+})
--- 5-calculator/src/utils/buttonsProps.ts
+++ 5-calculator/src/utils/buttonsProps.ts
@@ -1,95 +1,156 @@
 export const buttonsProps: {
   content: string
   styles: string
+  props?: {}
 }[] = [
   // First-Column
   {
     content: 'CE',
     styles: 'blackButton',
+    props: {
+      value: 'CANCEL_ENTRY',
+    },
   },
 
   {
     content: 'C',
     styles: 'blackButton',
+    props: {
+      value: 'CLEAR',
+    },
   },
   {
     content: `%`,
     styles: 'blackButton',
+    props: {
+      value: 'PERCENTAGE',
+    },
   },
   {
     content: '÷',
     styles: 'purpleButton',
+    props: {
+      value: 'DIVISION',
+    },
   },
   // Second-Column
   {
     content: '7',
     styles: 'blackButton',
+    props: {
+      value: '7',
+    },
   },
 
   {
     content: '8',
     styles: 'blackButton',
+    props: {
+      value: '8',
+    },
   },
   {
     content: `9`,
     styles: 'blackButton',
+    props: {
+      value: '9',
+    },
   },
   {
     content: 'X',
     styles: 'purpleButton',
+    props: {
+      value: 'MULTIPLICATION',
+    },
   },
   // Third-Column
   {
     content: '4',
     styles: 'blackButton',
+    props: {
+      value: '4',
+    },
   },
 
   {
     content: '5',
     styles: 'blackButton',
+    props: {
+      value: '5',
+    },
   },
   {
     content: `6`,
     styles: 'blackButton',
+    props: {
+      value: '6',
+    },
   },
   {
     content: '-',
     styles: 'purpleButton',
+    props: {
+      value: 'SUBTRACTION',
+    },
   },
   // Fourth-Column
   {
     content: '1',
     styles: 'blackButton',
+    props: {
+      value: '1',
+    },
   },
 
   {
     content: '2',
     styles: 'blackButton',
+    props: {
+      value: '2',
+    },
   },
   {
     content: `3`,
     styles: 'blackButton',
+    props: {
+      value: '3',
+    },
   },
   {
     content: '+',
     styles: 'purpleButton',
+    props: {
+      value: 'ADDITION',
+    },
   },
   // Fifth-Column
   {
-    content: '+-',
+    content: '±',
     styles: 'blackButton',
+    props: {
+      value: 'PLUS_MINUS',
+    },
   },
 
   {
     content: '0',
     styles: 'blackButton',
+    props: {
+      value: '0',
+    },
   },
   {
     content: `,`,
     styles: 'blackButton',
+    props: {
+      value: 'COMMA',
+    },
   },
   {
     content: '=',
     styles: 'equalsButton',
+    props: {
+      value: 'EQUAL',
+    },
   },
 ]
